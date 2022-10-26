40eb110a576dc2301ebea470231b2ccc1f8d44bf
michael-williamson <michaeldavid1721@yahoo.com>
2022-10-26 17:02:19 -0500

    unused code deletion

--- App.tsx
+++ App.tsx
@@ -1,6 +1,5 @@
 import { GiphySDK } from "@giphy/react-native-sdk";
 import React from "react";
-import { StyleSheet } from "react-native";
 import { keys } from "./keys";
 import { DrawerNavigation } from "./Src/Navigation/DrawerNavigation";
 import { CurrentMediaState } from "./Src/State_Management/CurrentMediaState";
@@ -24,24 +23,5 @@ const App: React.FC<Props> = () => {
     </CurrentMediaState>
   );
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: "600",
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: "400",
-  },
-  highlight: {
-    fontWeight: "700",
-  },
-});
 
 export default App;
